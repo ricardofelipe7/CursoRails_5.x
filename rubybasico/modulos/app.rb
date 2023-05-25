83125b26c7d82fe77d0733ef6e4a78ec10057891
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-25 17:31:13 -0300

    Reafactor: alterando modulos e variaves para estudos.

--- rubybasico/modulos/app.rb
+++ rubybasico/modulos/app.rb
@@ -2,17 +2,20 @@ require_relative 'pagamento'
 
 #include Pagamento
 
-puts Pagamento::PI
+#puts Pagamento::PI
 #puts PI
 
-puts "Digite a bandeira do cartão:"
-b = gets.chomp
+#puts "Digite a bandeira do cartão:"
+#b = gets.chomp
 
-puts "Digite o número do cartão:"
-n = gets.chomp
+#puts "Digite o número do cartão:"
+#n = gets.chomp
 
-puts "Digite o valor da compra:"
-v = gets.chomp
+#puts "Digite o valor da compra:"
+#v = gets.chomp
 
 #puts pagar(b, n , v)
-puts Pagamento::pagar(b, n, v)
+#puts Pagamento::pagar(b, n, v)
+
+p1 = Pagamento::Visa.new
+puts p1.pagando
